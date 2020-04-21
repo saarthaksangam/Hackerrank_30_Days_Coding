d334df3e6dfbb0829e26017c24db41314a8a962b
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-21 22:09:15 +0530

    Create Day_8_Dictionaries_and_Maps.py

---
+++ Day_8_Dictionaries_and_Maps.py
@@ -0,0 +1,14 @@
+n = int(input())
+d = dict()
+for i in range(0, n):
+    name, number = input().split()
+    d[name] = number
+for i in range(0, n):
+    try:
+        name = input()
+        if name in d:
+            print(f"{name}={d[name]}")
+        else:
+            print("Not found")
+    except:
+        break
